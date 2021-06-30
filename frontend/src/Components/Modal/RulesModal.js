af9f8dc898cabc083fb3f27286f02bc5c0f83564
kevinliu726 <b06902042@ntu.edu.tw>
2021-06-30 23:42:32 +0800

    Rules

--- frontend/src/Components/Modal/RulesModal.js
+++ frontend/src/Components/Modal/RulesModal.js
@@ -6,6 +6,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import "../../css/RulesModal.css";
+import { Grid } from "@material-ui/core";
 
 const RulesModal = ({ open, handleClose }) => {
   const classes = makeStyles(() => ({
@@ -60,6 +61,102 @@ const RulesModal = ({ open, handleClose }) => {
       </DialogTitle>
       <DialogContent className={classes.dialogContent}>
         <>
+          <div style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "8px" }}>Point of Cards:</div>
+          <table>
+            <tr>
+              <th style={{ fontSize: "18px", textAlign: "left" }}>Card</th>
+              <th style={{ fontSize: "18px", textAlign: "left" }}>Point</th>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src={require("../../cards/0.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+              </td>
+              <td style={{ fontSize: "18px", textAlign: "left" }}>1 or 11</td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src={require("../../cards/1.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+                <span style={{ fontSize: "20px" }}> ... </span>
+                <img
+                  src={require("../../cards/8.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+              </td>
+              <td style={{ fontSize: "18px", textAlign: "left" }}>2 ~ 9</td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src={require("../../cards/9.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+                <img
+                  src={require("../../cards/10.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+                <img
+                  src={require("../../cards/11.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+                <img
+                  src={require("../../cards/12.png").default}
+                  style={{ width: "50px", height: "50px", borderRadius: "10px" }}
+                  alt="cards"
+                ></img>
+              </td>
+              <td style={{ fontSize: "18px", textAlign: "left" }}>10</td>
+            </tr>
+          </table>
+          <ul style={{ fontSize: "18px", textAlign: "left" }}>
+            <li>The possible points of ( 2, J ) is [ 12 ].</li>
+            <li>The possible points of ( A, 3 ) is [ 4, 14 ].</li>
+            <li>The possible points of ( A, K, A ) is [ 12, 22, 32 ].</li>
+          </ul>
+          <div style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "8px" }}>Role:</div>
+          <ul style={{ fontSize: "18px" }}>
+            <li>Bank: The bank creates the room and battles with all coming players.</li>
+            <li>Player: The player battles with the bank of the room.</li>
+          </ul>
+          <div style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "8px" }}>Basic Gameflow:</div>
+          <ol style={{ fontSize: "18px" }}>
+            <li>Every player can set the bet.</li>
+            <li>
+              After every player sets the bet, everbody gets two cards. One is visible to others, and the other is
+              invisible.
+            </li>
+            <li>
+              During the turn of each player, one can choose “HIT” to ask for one more visible card or choose “STAND” to
+              end the turn.
+            </li>
+            <li>When all the players end their turns, the bank will show his hand cards.</li>
+            <li>
+              The bank can choose “HIT” to ask for one more visible card or choose “BATTLE” to battle with multiple
+              players. The invisible card of the player will be visible after the battle.
+            </li>
+            <li>If all the players are battled with bank, the game is over.</li>
+          </ol>
+          <div style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "8px" }}>Rules of GameFlow:</div>
+          <ol style={{ fontSize: "18px" }}>
+            <li>The player can’t “STAND” until one of possible points is greater than 14.</li>
+            <li>The player can’t “HIT” if the mininum of possible points is greater than 21.</li>
+            <li>The player can’t “HIT” if the number of cards is 5.</li>
+            <li>The bank can’t “BATTLE” untile one of possible points is greater than 14.</li>
+            <li>The bank can’t “HIT” if the minimum of possible points is greater than 21.</li>
+            <li>The bank can’t “HIT” if the number of cards is 5.</li>
+          </ol>
+          <div style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "8px" }}>Rules of Battle:</div>
           <div style={{ fontSize: "20px", fontWeight: "bold" }}>Definition</div>
           <ul style={{ fontSize: "18px" }}>
             <li>Explosion Cards: minimum of possible points is greater than 21</li>
@@ -179,7 +276,7 @@ const RulesModal = ({ open, handleClose }) => {
                 <img className="rcard" src={require("../../cards/1.png").default} alt="cards"></img>
                 <img className="rcard" src={require("../../cards/5.png").default} alt="cards"></img>
                 <img className="rcard" src={require("../../cards/3.png").default} alt="cards"></img>
-                <img className="rcard" src={require("../../cards/18.png").default} alt="cards"></img>
+                <img className="rcard" src={require("../../cards/44.png").default} alt="cards"></img>
               </div>
               <div style={{ fontSize: "18px", textAlign: "end" }}> Bonus: 3 times</div>
             </div>
